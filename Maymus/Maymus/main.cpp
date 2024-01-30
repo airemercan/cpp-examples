52473c898c928842a6a8e4667c3714688c9b555a
airemercan <iremercan08@hotmail.com>
2024-01-30 21:23:01 +0300

    Create maymus project

---
+++ Maymus/Maymus/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Maymus
+//
+//  Created by İrem Ercan on 30.01.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Mincap maymuşunu çok seviyorrr!!11!!!";
+    return 0;
+}
