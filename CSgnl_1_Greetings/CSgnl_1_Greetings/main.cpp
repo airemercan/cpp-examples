d2a27c2cc6bac781e295bea8aa4982a6d5838775
airemercan <iremercan08@hotmail.com>
2024-02-02 14:37:59 +0300

    greeting exercise

---
+++ CSgnl_1_Greetings/CSgnl_1_Greetings/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  CSgnl_1_Greetings
+//
+//  Created by İrem Ercan on 2.02.2024.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    string name;
+    std::cout << "Hi, please enter your name. \n";
+    std::cin >> name;
+    std::cout << "Hello, " << name << ".\n";
+    
+    return 0;
+}
+
+
+/*
+ Write a C++ program that prompts the user to enter their name and then prints a greeting message, “Hello, [name]!”.
+ */
