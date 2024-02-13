93467d9793bde4f2a9991d6f61d3de89d4c67121
airemercan <iremercan08@hotmail.com>
2024-02-13 22:48:25 +0300

    project started

---
+++ GreatFactorProject/GreatFactorProject/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GreatFactorProject
+//
+//  Created by İrem Ercan on 13.02.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
